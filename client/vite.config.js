959970738541c8e9ec3bc0913c6dd4e7cae9b57e
Andrei Alionte <andrei.alionte@gmail.com>
2024-11-01 10:03:48 +0200

    .

---
+++ client/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'; //from rollup bundler
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    visualizer({ open: true }), // Visualize bundle size
+  ],
+  build: {
+    minify: 'terser', // terser for minification
+    chunkSizeWarningLimit: 600, // chunk size warning limit
+    cssCodeSplit: true, // split css files or better caching
+    sourcemap: true, // enable sourcemaps for easier debugging
+  },
+})
